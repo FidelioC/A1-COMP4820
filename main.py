ff2a012bb346dd1471e979a80f5a3c2823df89a4
FidelioC <fiocheah08@gmail.com>
2024-09-27 18:48:53 -0500

    file process bruteforce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from Bio import SeqIO
 from Bio.Seq import Seq
+import click
 import re
 
 
@@ -10,21 +11,33 @@ def read_fasta_file():
     return seq_record
 
 
-def brute_force(sequence_text, sequence_pattern):
+def brute_force(sequence_text, sequence_list_patterns):
     index = 0
     count = 0
-    pattern_length = len(sequence_pattern)
     text_length = len(sequence_text)
 
-    while (index + pattern_length) < text_length:
-        # print(sequence_text.seq[index : (index + pattern_length)])
-        if sequence_text.seq[index : (index + pattern_length)] == sequence_pattern:
-            count += 1
+    while index < text_length:
+        # iterating the list of patterns
+        for sequence_pattern in sequence_list_patterns:
+            pattern_length = len(sequence_pattern)
+            if (index + pattern_length < text_length) and (
+                sequence_text.seq[index : (index + pattern_length)] == sequence_pattern
+            ):
+                count += 1
         index += 1
     return count
 
 
-if __name__ == "__main__":
-    sequence_pattern = "ATGA"
+@click.command()
+@click.option("--pattern", multiple=True)
+def commands_processing(pattern):
+    """command lines processing"""
+
     sequence_text = read_fasta_file()
-    print(brute_force(sequence_text, sequence_pattern))
+    sequence_list_patterns = pattern
+    print(sequence_list_patterns)
+    print(brute_force(sequence_text, sequence_list_patterns))
+
+
+if __name__ == "__main__":
+    commands_processing()
