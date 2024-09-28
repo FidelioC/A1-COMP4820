43b6f632721280419471dea5fb996c2dd841ab1e
FidelioC <fiocheah08@gmail.com>
2024-09-27 19:13:07 -0500

    add output format

--- main.py
+++ main.py
@@ -1,42 +1,62 @@
 from Bio import SeqIO
-from Bio.Seq import Seq
 import click
-import re
 
 
 def read_fasta_file():
     seq_record = SeqIO.read("Sorangium_cellulosum_19lines.fasta", "fasta")
-    simple_seq = Seq("ATGA")
 
     return seq_record
 
 
-def brute_force(sequence_text, sequence_list_patterns):
+def brute_force(sequence_text, sequence_list_patterns, dictionary):
     index = 0
     count = 0
     text_length = len(sequence_text)
 
+    # going over char by char
     while index < text_length:
         # iterating the list of patterns
         for sequence_pattern in sequence_list_patterns:
             pattern_length = len(sequence_pattern)
+            # check if they're the same pattern
             if (index + pattern_length < text_length) and (
                 sequence_text.seq[index : (index + pattern_length)] == sequence_pattern
             ):
                 count += 1
+                dictionary[sequence_pattern].append(index)
         index += 1
-    return count
+    return dictionary
+
+
+def initialize_dictionary(sequence_list_patterns):
+    dictionary = {}
+    for pattern in sequence_list_patterns:
+        if pattern not in dictionary:
+            dictionary[pattern] = []
+    return dictionary
+
+
+def print_output(dictionary):
+    # print(dictionary)
+    for item in dictionary:
+        print(
+            f"Pattern: {item} \n\tCount: {len(dictionary[item])} \n\tLocations: {dictionary[item]}"
+        )
 
 
 @click.command()
 @click.option("--pattern", multiple=True)
 def commands_processing(pattern):
-    """command lines processing"""
-
+    # init
     sequence_text = read_fasta_file()
     sequence_list_patterns = pattern
-    print(sequence_list_patterns)
-    print(brute_force(sequence_text, sequence_list_patterns))
+    dictionary = initialize_dictionary(sequence_list_patterns)
+
+    # algorithms
+    dictionary = brute_force(sequence_text, sequence_list_patterns, dictionary)
+
+    # output
+    print_output(dictionary)
 
 
 if __name__ == "__main__":
